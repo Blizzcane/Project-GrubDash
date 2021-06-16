d493f0db7780cfabdce1f98d3f25f60a08475273
Blizzcane <blizzcane@gmail.com>
2021-06-15 19:21:17 -0500

    added list function to orders controller

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,9 +1,17 @@
 const path = require("path");
 
-// Use the existing order data
+// Use the existing dishes data
 const orders = require(path.resolve("src/data/orders-data"));
 
-// Use this function to assigh ID's when necessary
+// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+// TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res) {
+  res.json({data: orders });
+}
+ 
+module.exports = {
+  list
+};
